7269e425900bb9a7a54828b2da2224c36e5a852f
Ben <Benjamin.w.dahlke@gmail.com>
2017-08-03 15:35:23 -0400

    Drag and drop

---
+++ BenDragAndDrop/BenDragAndDrop/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Ben Drag And Drop
+//
+//  Created by Ben on Thursday, August 3, 2017
+//  Copyright (c) Ben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StandaloneCodeaViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) StandaloneCodeaViewController *viewController;
+
+@end
